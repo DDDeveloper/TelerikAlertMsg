3ab7cb5d7070fbabfb79783fbd4ba36d2393e311
DDDeveloper <look-deadnote@hotmail.com>
2016-05-03 14:12:53 +0700

    Update TelerikAlertMsg.aspx.vb

--- TelerikAlertMsg.aspx.vb
+++ TelerikAlertMsg.aspx.vb
@@ -1,1 +1,1 @@
-RadWindowManager1.RadAlert("Please Upload Document", 250, 200, "My Alert", "callBackFn", "myAlertImage.png")
+RadWindowManager1.RadAlert("msg", 250, 200, "My Alert", "callBackFn", "myAlertImage.png")
